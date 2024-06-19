d0297c16a84d497f70272272a8e1ec96769a379b
GERARD2619-17 <gera121134@gmail.com>
2024-06-19 11:12:11 -0600

    agregando trafos

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ document.getElementById('agregar').addEventListener('click', () => {
     const equipo = document.getElementById('equipos').value;
     const cantidad = document.getElementById('cantidad').value;
     if (equipo && cantidad) {
-        const lista = document.querySelector('.lista');
+        const lista = document.getElementById('lista-equipos');
         const item = document.createElement('div');
+        item.classList.add('list-item');
         item.textContent = `${cantidad} - ${equipo}`;
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
+        deleteButton.classList.add('delete-button');
         deleteButton.addEventListener('click', () => item.remove());
         item.appendChild(deleteButton);
         lista.appendChild(item);
