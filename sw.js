ff0a293c1edabc9ae3378438be3da7871ea4b8b8
GERARD2619-17 <gera121134@gmail.com>
2024-06-21 10:25:31 -0600

    prueba1

--- sw.js
+++ sw.js
@@ -40,3 +40,20 @@ self.addEventListener('activate', event => {
         })
     );
 });
+self.addEventListener("install", e=> {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./",
+             "./src/master.css",
+             "./images/logoredondosintransparencia192x192.png"])
+        })
+    );
+});
+
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch(e.request);
+        })
+    );
+});
